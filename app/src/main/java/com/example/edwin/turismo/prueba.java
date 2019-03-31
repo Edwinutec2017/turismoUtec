56027e8ca9fd837ede0c807601e1326a55a71a6f
edwin <edwinalexandernolasco@gmail.com>
2019-03-30 21:07:58 -0600

    prueba
    prueba

---
+++ app/src/main/java/com/example/edwin/turismo/prueba.java
@@ -0,0 +1,17 @@
+package com.example.edwin.turismo;
+
+/**
+ * Created by Edwin on 30/03/2019.
+ */
+
+public class prueba {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
